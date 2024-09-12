09266023bf4164165c8f6be4387d3fcbd0f51ece
muhammadatoqeer <muhammadanstoqeer@gmail.com>
2024-09-12 14:15:21 +0000

    added introToJS.js

---
+++ 01_basics/introToJS.js
@@ -0,0 +1,14 @@
+// A script is basically a set of instructions that you give to a computer to perform.
+
+// Each step of the script is known as a statement/instruction. Each instruction usually starts on a new line (Good Code readability practices).
+
+// Code blocks are what's between the two curly braces.
+
+// Single-line comment is added by double forward slash
+
+
+/*
+Multi-line comment can be added using the HTML format
+*/
+
+// Keywords are reserved set of words by the programming language which cannot be used out of the context of their use. e.g. function, let, var, const etc. 
