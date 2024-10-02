3ce4a1f655415331f023e8c3ee556d1fd2e456e4
muhammadatoqeer <muhammadanstoqeer@gmail.com>
2024-10-02 12:01:34 +0500

    dom notes

---
+++ 06_dom/02-elements-selection.js
@@ -0,0 +1,3 @@
+document.getElementById('Name of the ID') //targets that specific element directly because IDs are unique for HTML Elements.
+
+document.getElementsByClassName('name of class') //returns a collection of the results (could think like an array) and you then use an index to specify which className you want to target 
